ef5bb57b9caed9bbaa89d41cc80b47059717e14a
Dionel22 <diondiaz293@gmail.com>
2023-05-20 19:05:52 -0300

    Express_Sequelize

--- api/src/db.js
+++ api/src/db.js
@@ -31,8 +31,8 @@ sequelize.models = Object.fromEntries(capsEntries);
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
 const { Recipe, Diet } = sequelize.models;
-Recipe.belongsToMany(Diet, { through: 'Recipe_Diet' , timestamps: false})
-Diet.belongsToMany(Recipe, { through: 'Recipe_Diet' , timestamps: false})
+Recipe.belongsToMany(Diet, { through: 'Recipe_Diet' , timestamps: false});
+Diet.belongsToMany(Recipe, { through: 'Recipe_Diet' , timestamps: false});
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
