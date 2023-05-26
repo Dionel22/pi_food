6dcf1948b78dcf5f2c252222ce2044949c6cebfe
Dionel22 <diondiaz293@gmail.com>
2023-05-26 01:39:09 -0300

    React_Redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import LandingPage from './views/LandingPage/LandingPage';
+import Home from './views/Home/Home';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={LandingPage}/>
+      <Route exact path="/home" component={Home}/>
     </Switch>
   );
 }
